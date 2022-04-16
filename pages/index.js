09543f58fcdbdf7f1194e9f762c65d58668e8e4c
wizard <wizardlabsxyz@gmail.com>
2022-04-16 14:58:32 -0700

    Basic Setup and material UI

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
-import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { ethers } from "ethers";
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    const [val, setVal] = useState(undefined);
 
-      <Footer />
-    </div>
-  )
+    main().then((data) => {
+        console.log(data.toString());
+        setVal(data.toString());
+    })
+
+    async function main() {
+        var url = 'https://eth-rinkeby.alchemyapi.io/v2/Bk4gFDQ7onQkwQ8EhCQh7FRZIFySm0Vv';
+        const provider = new ethers.providers.JsonRpcProvider(url);
+        const balance = await provider.getBalance("0x9DAfB9860FcD9A1E1dc00b4660E65cEcDd88E4bB")
+
+        return balance;
+    }
+
+
+    return (
+        <div className="container">
+            <main>
+                <Header title="Welcome to WizardLabsDemoNFT" />
+                <Button variant="contained">Mint</Button>
+            </main>
+        </div>
+    );
 }
