51a5e753fd4248d0816a45ccebd38a712c5cb954
wizard <wizardlabsxyz@gmail.com>
2022-04-16 22:47:37 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import _ from 'lodash';
-import Header from '@components/Header'
 
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -110,91 +108,91 @@ export default function Home() {
                 </DialogContent>
             </Dialog>
             <body>
-                <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navigation w-nav"></div>
-                <div class="section cc-store-home-wrap">
-                    <div class="intro-header">
-                        <div class="intro-content cc-homepage">
-                            <div class="intro-text">
-                                <div class="heading-jumbo">Grow your business.</div>
-                                <div class="paragraph-bigger cc-bigger-white-light">Give your business a boost with a beautifully crafted homepage.<br/></div>
+                <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" className="navigation w-nav"></div>
+                <div className="section cc-store-home-wrap">
+                    <div className="intro-header">
+                        <div className="intro-content cc-homepage">
+                            <div className="intro-text">
+                                <div className="heading-jumbo">Grow your business.</div>
+                                <div className="paragraph-bigger cc-bigger-white-light">Give your business a boost with a beautifully crafted homepage.<br/></div>
                             </div>
                             <div id="custom-bo"></div>
                         </div>
                     </div>
-                    <div class="container">
-                        <div class="motto-wrap">
-                            <div class="label cc-light">What we believe in</div>
-                            <div class="heading-jumbo-small">Grow your business, establish your brand, and put your customers first.<br/></div>
+                    <div className="container">
+                        <div className="motto-wrap">
+                            <div className="label cc-light">What we believe in</div>
+                            <div className="heading-jumbo-small">Grow your business, establish your brand, and put your customers first.<br/></div>
                         </div>
-                        <div class="divider"></div>
-                        <div class="home-content-wrap">
-                            <div class="w-layout-grid about-grid">
+                        <div className="divider"></div>
+                        <div className="home-content-wrap">
+                            <div className="w-layout-grid about-grid">
                                 <div id="w-node-_86e64837-0616-515b-4568-76c147234d34-8257984c">
-                                    <div class="home-section-wrap">
-                                        <div class="label cc-light">About</div>
-                                        <h2 class="section-heading">Who we are</h2>
-                                        <p class="paragraph-light">Nulla vel sodales tellus, quis condimentum enim. Nunc porttitor venenatis feugiat. Etiam quis faucibus erat, non accumsan leo. Aliquam erat volutpat. Vestibulum ac faucibus eros. Cras ullamcorper gravida tellus ut consequat.</p>
+                                    <div className="home-section-wrap">
+                                        <div className="label cc-light">About</div>
+                                        <h2 className="section-heading">Who we are</h2>
+                                        <p className="paragraph-light">Nulla vel sodales tellus, quis condimentum enim. Nunc porttitor venenatis feugiat. Etiam quis faucibus erat, non accumsan leo. Aliquam erat volutpat. Vestibulum ac faucibus eros. Cras ullamcorper gravida tellus ut consequat.</p>
                                     </div>
-                                    <a href="/about" class="button w-inline-block">
+                                    <a href="/about" className="button w-inline-block">
                                         <div>Learn More</div>
                                     </a>
                                 </div><img src="../images/placeholder-3.svg" id="w-node-_86e64837-0616-515b-4568-76c147234d3f-8257984c" alt=""/>
                             </div>
-                            <div class="w-layout-grid about-grid cc-about-2">
+                            <div className="w-layout-grid about-grid cc-about-2">
                                 <div id="w-node-_86e64837-0616-515b-4568-76c147234d41-8257984c">
-                                    <div class="home-section-wrap">
-                                        <div class="label cc-light">Team</div>
-                                        <h2 class="section-heading">What we do</h2>
-                                        <p class="paragraph-light">Nulla vel sodales tellus, quis condimentum enim. Nunc porttitor venenatis feugiat. Etiam quis faucibus erat, non accumsan leo. Aliquam erat volutpat. Vestibulum ac faucibus eros. Cras ullamcorper gravida tellus ut consequat.</p>
+                                    <div className="home-section-wrap">
+                                        <div className="label cc-light">Team</div>
+                                        <h2 className="section-heading">What we do</h2>
+                                        <p className="paragraph-light">Nulla vel sodales tellus, quis condimentum enim. Nunc porttitor venenatis feugiat. Etiam quis faucibus erat, non accumsan leo. Aliquam erat volutpat. Vestibulum ac faucibus eros. Cras ullamcorper gravida tellus ut consequat.</p>
                                     </div>
-                                    <a href="/team" class="button w-inline-block">
+                                    <a href="/team" className="button w-inline-block">
                                         <div>Learn More</div>
                                     </a>
-                                </div><img src="../images/placeholder-1.svg" id="w-node-_86e64837-0616-515b-4568-76c147234d4c-8257984c" alt=""/>
+                                </div><img />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="section">
-                    <div class="container">
-                        <div class="blog-heading">
-                            <div class="label cc-light">About Us</div>
-                            <h2 class="work-heading">Company news</h2>
+                <div className="section">
+                    <div className="container">
+                        <div className="blog-heading">
+                            <div className="label cc-light">About Us</div>
+                            <h2 className="work-heading">Company news</h2>
                         </div>
-                        <div class="collection-list-wrapper w-dyn-list">
-                            <div role="list" class="collection-wrap w-dyn-items">
-                                <div role="listitem" class="blog-preview-wrap w-dyn-item">
-                                    <a href="#" class="business-article-heading"></a>
-                                    <div class="label cc-blog-date"></div>
-                                    <p class="paragraph-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                        <div className="collection-list-wrapper w-dyn-list">
+                            <div role="list" className="collection-wrap w-dyn-items">
+                                <div role="listitem" className="blog-preview-wrap w-dyn-item">
+                                    <a href="#" className="business-article-heading"></a>
+                                    <div className="label cc-blog-date"></div>
+                                    <p className="paragraph-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
                                 </div>
                             </div>
-                            <div class="status-message cc-no-data w-dyn-empty">
+                            <div className="status-message cc-no-data w-dyn-empty">
                                 <div>No items found.</div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="section cc-cta">
-                    <div class="container">
-                        <div class="cta-wrap">
+                <div className="section cc-cta">
+                    <div className="container">
+                        <div className="cta-wrap">
                             <div>
-                                <div class="cta-text">
-                                    <div class="heading-jumbo-small">Grow your business.<br/></div>
-                                    <div class="paragraph-bigger cc-bigger-light">Today is the day to build the business of your dreams. Share your mission with the world — and blow your customers away.<br/></div>
+                                <div className="cta-text">
+                                    <div className="heading-jumbo-small">Grow your business.<br/></div>
+                                    <div className="paragraph-bigger cc-bigger-light">Today is the day to build the business of your dreams. Share your mission with the world — and blow your customers away.<br/></div>
                                 </div>
-                                <a href="#" class="button cc-jumbo-button w-inline-block">
+                                <a href="#" className="button cc-jumbo-button w-inline-block">
                                     <div>Start Now</div>
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="section">
-                    <div class="container">
-                        <div class="footer-wrap">
-                            <a href="https://webflow.com/" target="_blank" class="webflow-link w-inline-block"><img src="images/webflow-w-small2x_1webflow-w-small2x.png" width="15" alt="" class="webflow-logo-tiny"/>
-                                <div class="paragraph-tiny">Powered by Webflow</div>
+                <div className="section">
+                    <div className="container">
+                        <div className="footer-wrap">
+                            <a href="https://webflow.com/" target="_blank" className="webflow-link w-inline-block"><img src="images/webflow-w-small2x_1webflow-w-small2x.png" width="15" alt="" className="webflow-logo-tiny"/>
+                                <div className="paragraph-tiny">Powered by Webflow</div>
                             </a>
                         </div>
                     </div>
